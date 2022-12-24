1419629ffe6ff88c331e3dffa0720681df1b5332
huseyinbabal <huseyinbabal88@gmail.com>
2022-12-25 00:18:06 +0300

    yaml format added

--- cmd/yaml.go
+++ cmd/yaml.go
@@ -27,6 +27,17 @@ var yamlBeautifyCmd = &cobra.Command{
 	},
 }
 
+var yamlFormatCmd = &cobra.Command{
+	Use:   "format",
+	Short: "Formats yaml",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		y, err := yaml.Format(yamlValue)
+		if err != nil {
+			return err
+		}
+		return quick.Highlight(os.Stdout, y, "yaml", "terminal256", "monokai")
+	},
+}
 var yamlToJsonCmd = &cobra.Command{
 	Use:   "toJson",
 	Short: "Converts yaml into json",
@@ -51,7 +62,13 @@ func init() {
 	if err != nil {
 		log.Fatalf("Please provide a valid yaml with --value parameter")
 	}
+	yamlFormatCmd.Flags().StringVar(&yamlValue, "value", "", "YAML string")
+	err = yamlFormatCmd.MarkFlagRequired("value")
+	if err != nil {
+		log.Fatalf("Please provide a valid yaml with --value parameter")
+	}
 	yamlCmd.AddCommand(yamlBeautifyCmd)
 	yamlCmd.AddCommand(yamlToJsonCmd)
+	yamlCmd.AddCommand(yamlFormatCmd)
 	rootCmd.AddCommand(yamlCmd)
 }
